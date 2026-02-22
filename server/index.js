fb2ae9fd46c541fffd54e94d3976ad0d3e538be6
SHAIKHANIF2004 <shaikhanif2004@gmail.com>
2026-02-22 14:59:58 +0530

    Update CORS origin to production URL

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.post(
 //middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://ai-notes-generator-client.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   }),
